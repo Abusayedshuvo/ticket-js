3d4401744934550ea4e50c8eee789b671399917b
Abusayedshuvo <abusayedshuvo24@gmail.com>
2024-10-02 12:40:05 +0600

    grand total, total, not buy more than 4 ticket

--- assest/script.js
+++ assest/script.js
@@ -2,7 +2,6 @@ let buySeat = [];
 let uniqueSeat = [];
 
 function seatClick(seatId) {
-  getBgColorById(seatId);
   buySeat.push(seatId);
   if (buySeat.length > 0) {
     for (let i of buySeat) {
@@ -12,7 +11,18 @@ function seatClick(seatId) {
       }
     }
   }
+
+  if (uniqueSeat.length >= 4) {
+    const allButton = document.querySelectorAll("#all-seat button");
+    for (let i of allButton) {
+      i.setAttribute("disabled", true);
+    }
+    alert("you can't buy more than 4 ticket");
+  }
+  getBgColorById(seatId);
   setTextById("seat-count", uniqueSeat.length);
   setTextById("available-seat", 40 - uniqueSeat.length);
   setTextById("total-price", uniqueSeat.length * 550);
+  setTextById("grand-total", uniqueSeat.length * 550);
+  copupon(uniqueSeat);
 }
--- assest/utili.js
+++ assest/utili.js
@@ -23,6 +23,25 @@ function newTrCreate(elementId) {
   const newTr = document.createElement("tr");
   newTr.innerHTML = `<td class="uppercase">${elementId} </td>
                     <td>Economoy</td>
-                    <td>550</td>`;
+                    <td class="text-right">550</td>`;
   return seatTable.appendChild(newTr);
+}
+
+// copupon code function
+function copupon(uniqueSeat) {
+  const applyBtn = document.getElementById("apply-btn");
+  if (uniqueSeat.length >= 4) {
+    applyBtn.removeAttribute("disabled");
+  }
+  applyBtn.addEventListener("click", function () {
+    const copuponInput = document.getElementById("copupon-input").value;
+    const totalPrice = getTextById("total-price");
+    let grandTotal = 0;
+    if (copuponInput === "NEW15") {
+      grandTotal = totalPrice - totalPrice * 0.15;
+    } else if (copuponInput === "Couple20") {
+      grandTotal = totalPrice - totalPrice * 0.2;
+    }
+    setTextById("grand-total", grandTotal);
+  });
 }
